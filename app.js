ebb404b9813c891dc09b9536bc30e3dd5422b96c
Minato <ankamasish@gmail.com>
2022-12-21 09:25:55 +0530

    post reuest working

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
-const path=require("path");
-const express=require("express");
-const cors=require("cors");
-const app=express();
-const User=require("./models/user");
-const sequelize=require("./util/db");
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const User = require("./models/user");
+const sequelize = require("./util/db");
 app.use(cors());
 app.use(express.json());
 
-app.post("/task",async(req,res)=>{
-    const {name,}
+app.post("/task", async (req, res) => {
+    const {
+        name,
+        firstName,
+        lastName,
+        dateofbirth,
+        address,
+        email,
+        mobile_number
+    } = req.body;
+    const data=await User.create({
+        name,
+        firstName,
+        lastName,
+        dateofbirth,
+        address,
+        email,
+        mobile_number
+    });
+    res.status(201).json({
+        newUserDetail:data
+    })
 })
 
 
 
 sequelize.sync()
-.then(()=>{
-app.listen(3000)
-})
-.catch((err)=>{
-    console.log(err);
-})
+    .then(() => {
+        app.listen(3000)
+    })
+    .catch((err) => {
+        console.log(err);
+    })
--- util/db.js
+++ util/db.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize")
 
 const sequelize=new Sequelize("node","root","123456789",{
-    host:localhost,
-    dialect:root
+   dialect:"mysql",
+    host:"localhost"
 })
 
 module.exports=sequelize;
